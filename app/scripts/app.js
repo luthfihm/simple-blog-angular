ac5b01ce9e552d5b47d1cbbd55069311dd5ef6b4
Luthfi Hamid Masykuri <mail@luthfihm.com>
2016-02-21 17:59:29 +0700

    Create Project from Yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name simpleBlogApp
+ * @description
+ * # simpleBlogApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('simpleBlogApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simpleBlogApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the simpleBlogApp
+ */
+angular.module('simpleBlogApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
